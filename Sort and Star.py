24492a995727920e7d9f05e8b81c9720563e93b7
vsmrnw <102534967+vsmrnw@users.noreply.github.com>
2022-06-01 12:01:06 +0300

    Create Sort and Star.py

---
+++ Sort and Star.py
@@ -0,0 +1,15 @@
+'''You will be given a vector of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+The returned value must be a string, and have "***" between each of its letters.
+
+You should not remove or add elements from/to the array.'''
+def two_sort(array):
+    array.sort()
+    a = ''
+    for x in array[0]:
+        for y in list(x):            
+            a += a.join(x)
+            a += ('***')
+    
+    return(a[:-3:])
+        
